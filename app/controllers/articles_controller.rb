004eab6f38dd194fd33b7e18d7d0ca4812ff73c3
Kazuho Suzuki <kazu.com.0203@gmail.com>
2021-03-11 16:39:04 +0900

    board編集機能、削除機能一部追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,14 +10,29 @@ class ArticlesController < ApplicationController
  def create
     @board = current_user.boards.build(board_params)
     if @board.save
-        redirect_to new_article_path(@board)
+        redirect_to new_article_path(@board), notice: '保存出来ました'
     else
+        flash.now[:error] = '保存出来ませんでした'
         render :new
     end
  end
 
  def show
     @board = Board.find(params[:id])
+ end
+
+ def edit
+    @board = current_user.boards.find(params[:id])
+ end
+
+ def update
+    @board = Board.find(params[:id])
+    if @board.update(board_params)
+        redirect_to article_path(@board), notice: '更新出来ました'
+    else
+        flash.now[:error] = '更新出来ませんでした'
+        render :edit
+    end
  end
 
  private
