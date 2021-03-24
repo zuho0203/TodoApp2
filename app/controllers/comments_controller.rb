7281eaf8f6139b237b7e4e77cadfb73fcaf3c495
Kazuho Suzuki <kazu.com.0203@gmail.com>
2021-03-24 21:13:11 +0900

    コメント機能作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,17 @@
 class CommentsController < ApplicationController
     def new
-        board = Board.find(params[:board_id])
+        @board = Board.find(params[:board_id])
         task = Task.find(params[:task_id])
         @comment = task.comments.build
     end
 
     def create
+        @board = Board.find(params[:board_id])
+        task = Task.find(params[:task_id])
         @comment = task.comments.build(comment_params)
+        @comment.user_id = current_user.id
         if @comment.save
-            redirect_to board_task_path(@comment.task), notice: 'コメントを追加'
+            redirect_to board_task_path(@board.id, @comment.task), notice: 'コメントを追加'
         else
             flash.now[:error] = '更新出来ませんでした'
             render :new
